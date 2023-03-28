20d633c8f6e5d4d2fa8dab6f161d2cfef4513ef7
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-03-28 17:45:45 +0530

    update packege
    I'm update yarn package and yarn files, package json file and others

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const Home = () => {
   return (
     <>
@@ -22,7 +23,6 @@ const Home = () => {
                     <i className='fab fa-youtube youtube'></i>
                     <i className='fab fa-pinterest pinterest'></i>
                     <i className='fab fa-dribbble dribbble'></i>
-
                 </div>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                 <button className="primary-btn">Contact Us</button>
